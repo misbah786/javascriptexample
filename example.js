c6e5e8b332795d40b8556cb8f319d52c80b4dbad
misbah786 <misbah786shaik@gmail.com>
2021-02-16 14:33:13 +0530

    add hello world program

--- example.js
+++ example.js
@@ -1,12 +1,9 @@
-<!DOCTYPE html> 
-<html> 
-  <head> 
-    <title></title> 
-  </head> 
-  <body bgcolor="white"> 
-   <p>Paragraph 1</p> 
-   <script type="text/javascript"> 
-   document.bgColor ="lightblue"; 
-   </script> 
-  </body> 
-<html> 
+<html>
+<body>
+
+<script>
+console.log(5 + 6);
+</script>
+
+</body>
+</html>
